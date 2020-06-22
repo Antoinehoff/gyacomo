9530ffa47e82667fdd9423df996f8fddd860ed91
Antoine Cyril David Hoffmann <ahoffman@spcpc606.epfl.ch>
2020-06-22 17:45:16 +0200

    matlab scripts for post processing

---
+++ matlab/default_plots_options.m
@@ -0,0 +1,9 @@
+%% Default values for plots
+set(0,'defaulttextInterpreter','latex') 
+set(0, 'defaultAxesTickLabelInterpreter','latex');
+set(0, 'defaultLegendInterpreter','latex');
+set(0,'defaultAxesFontSize',16)
+set(0, 'DefaultLineLineWidth', 1.5);
+line_colors = [[0, 0.4470, 0.7410];[0.8500, 0.3250, 0.0980];[0.9290, 0.6940, 0.1250];...
+    [0.4940, 0.1840, 0.5560];[0.4660, 0.6740, 0.1880];[0.3010, 0.7450, 0.9330];[0.6350, 0.0780, 0.1840]];
+line_styles = {'-','-.','--',':'};
