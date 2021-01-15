b82803a09f196d5b8d3a7d0cabaadc19752bd52d
Antoine Cyril David Hoffmann <ahoffman@spcpc606.epfl.ch>
2021-01-15 14:34:16 +0100

    moments boundaries are extended for closure

--- src/memory.F90
+++ src/memory.F90
@@ -12,18 +12,18 @@ SUBROUTINE memory
   IMPLICIT NONE
 
   ! Moments and moments rhs
-  CALL allocate_array(     moments_e, ips_e,ipe_e, ijs_e,ije_e, ikrs,ikre, ikzs,ikze, 1,ntimelevel )
-  CALL allocate_array(     moments_i, ips_i,ipe_i, ijs_i,ije_i, ikrs,ikre, ikzs,ikze, 1,ntimelevel )
+  CALL allocate_array(     moments_e, ips_e-2,ipe_e+2, ijs_e-1,ije_e+1, ikrs,ikre, ikzs,ikze, 1,ntimelevel )
+  CALL allocate_array(     moments_i, ips_i-2,ipe_i+2, ijs_i-1,ije_i+1, ikrs,ikre, ikzs,ikze, 1,ntimelevel )
   CALL allocate_array( moments_rhs_e, ips_e,ipe_e, ijs_e,ije_e, ikrs,ikre, ikzs,ikze, 1,ntimelevel )
   CALL allocate_array( moments_rhs_i, ips_i,ipe_i, ijs_i,ije_i, ikrs,ikre, ikzs,ikze, 1,ntimelevel )
 
   ! Electrostatic potential
   CALL allocate_array(phi, ikrs,ikre, ikzs,ikze)
 
-  ! Electron kernel evaluation
-  CALL allocate_array(Kernel_e, ijs_e,ije_e, ikrs,ikre, ikzs,ikze)
+  ! Electron kernel evaluation from ij=0 to jmax+1 for truncation
+  CALL allocate_array(Kernel_e, ijs_e-1,ije_e+1, ikrs,ikre, ikzs,ikze)
   ! Ion kernel evaluation
-  CALL allocate_array(Kernel_i, ijs_i,ije_i, ikrs,ikre, ikzs,ikze)
+  CALL allocate_array(Kernel_i, ijs_i-1,ije_i+1, ikrs,ikre, ikzs,ikze)
 
   ! Collision matrix
   IF (CO .EQ. -1) THEN
