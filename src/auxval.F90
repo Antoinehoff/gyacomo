bd7c9676eebc8b37c5984bcaa6d330cad735454b
Antoine Cyril David Hoffmann <ahoffman@spcpc606.epfl.ch>
2021-03-15 16:11:50 +0100

    added output of the parallel setup in terminal

--- src/auxval.F90
+++ src/auxval.F90
@@ -26,14 +26,33 @@ subroutine auxval
 
   CALL memory ! Allocate memory for global arrays
 
+  !! Display parallel settings
   DO i_ = 0,num_procs-1
     CALL mpi_barrier(MPI_COMM_WORLD, ierr)
     IF (my_id .EQ. i_) THEN
-      WRITE (*,'(I2,A9,I3,A8,I3,A8,I3,A8,I3,A15,I6)') &
-        i_,': ikrs = ', ikrs, ' ikre = ', ikre, 'ikzs = ', ikzs, ' ikze = ', ikze
-      WRITE (*,'(A14,I4,A10,I4,A15,I6)') &
-        '  local_nkr = ',local_nkr,' offset = ',local_nkr_offset,' alloc_local = ', alloc_local_1+alloc_local_2
+      IF (my_id .EQ. 0) WRITE(*,*) ''
+      IF (my_id .EQ. 0) WRITE(*,*) '--------- Parallel environement ----------'
+      IF (my_id .EQ. 0) WRITE(*,'(A9,I3,A10,I3,A10,I3)') 'n_procs= ', num_procs, ', num_procs_p   = ', num_procs_p, ', num_procs_kr   = ', num_procs_kr
+      IF (my_id .EQ. 0) WRITE(*,*) ''
+      WRITE(*,'(A9,I3,A10,I3,A10,I3)')&
+       'my_id  = ', my_id, ', rank_p  = ', rank_p, ', rank_r  = ', rank_r
+       WRITE(*,'(A22,I3,A10,I3)')&
+       '              ips_e = ', ips_e, ', ikrs  = ', ikrs
+       WRITE(*,'(A22,I3,A10,I3)')&
+       '              ipe_e = ', ipe_e, ', ikre  = ', ikre
+       WRITE(*,'(A22,I3,A10,I3)')&
+       '              ips_i = ', ips_i, ', ikzs  = ', ikzs
+       WRITE(*,'(A22,I3,A10,I3)')&
+       '              ipe_i = ', ipe_i, ', ikze  = ', ikze
+
+      !  WRITE(*,'(A9,I3,A10,I3,A10,I3,A10,I3)')&
+      !   ' ips_e = ',ips_e,', ipe_e = ',ipe_e,', ips_i = ',ips_i,', ipe_i = ',ipe_i
+      ! WRITE (*,'(A9,I3,A10,I3,A10,I3,A10,I3,A10,I3)') &
+      ! ' ikrs  = ', ikrs, ', ikre  = ', ikre, ', ikzs  = ', ikzs, ', ikze = ', ikze
+      IF (my_id .NE. num_procs-1) WRITE (*,*) ''
+      IF (my_id .EQ. num_procs-1) WRITE(*,*) '------------------------------------------'
     ENDIF
   ENDDO
+  CALL mpi_barrier(MPI_COMM_WORLD, ierr)
 
 END SUBROUTINE auxval
