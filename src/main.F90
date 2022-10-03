b7f629a5e072b48fe7b991ec02d334032748ad82
Antoine Hoffmann <antoine.hoffmann@epfl.ch>
2022-10-03 18:08:58 +0200

    added copyright to the top of the code

--- src/main.F90
+++ src/main.F90
@@ -1,7 +1,10 @@
+!GYACOMO (Gyrokinetic Advanced Collision Moment solver, 2021)
+!Copyright (C) 2022  A.C.D. Hoffmann
+
 PROGRAM main
   !    Main program
 
-  use prec_const 
+  use prec_const
   IMPLICIT NONE
 
   CALL control
