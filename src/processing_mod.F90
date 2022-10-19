f7125b3d4df45799e48a8244476cc9d4cbc8e0d0
Antoine Hoffmann <antoine.hoffmann@epfl.ch>
2022-10-19 18:42:52 +0200

    comment the correction operator for heat flux

--- src/processing_mod.F90
+++ src/processing_mod.F90
@@ -370,8 +370,8 @@ SUBROUTINE compute_radial_electron_heatflux
     ENDDO
   ENDIF
   ! Add polarisation contribution
-  integrant(izs:ize) = integrant(izs:ize) + tau_e*imagu*ky_&
-  *CONJG(phi(iky,ikx,izs:ize))*phi(iky,ikx,izs:ize) * HF_phi_correction_operator(iky,ikx,izs:ize)
+  ! integrant(izs:ize) = integrant(izs:ize) + tau_e*imagu*ky_&
+  ! *CONJG(phi(iky,ikx,izs:ize))*phi(iky,ikx,izs:ize) * HF_phi_correction_operator(iky,ikx,izs:ize)
   ! Integrate over z
   integrant(izgs:izge) = Jacobian(izgs:izge,0)*integrant(izgs:izge)
   call simpson_rule_z(integrant,integral)
